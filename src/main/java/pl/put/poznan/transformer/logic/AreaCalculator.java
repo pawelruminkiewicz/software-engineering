e6048b4566d2185887145d222583bee99070be31
pablo <pawel@ruminkiewicz.pl>
2017-12-29 19:12:10 +0100

    Task #5547: Wyznaczenie powierzchni dla pojedynczych pomieszczeń

--- src/main/java/pl/put/poznan/transformer/logic/AreaCalculator.java
+++ src/main/java/pl/put/poznan/transformer/logic/AreaCalculator.java
@@ -13,6 +13,9 @@ public class AreaCalculator implements Visitor {
 
     @Override
     public void visit(Room location) {
-
+        float x = location.getX();
+        float y = location.getY();
+        float area = x*y;
+        location.setArea(area);
     }
 }
