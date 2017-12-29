54b86a8da2c0bec55a15b96bdf0e9984a91c007a
inf127217 <karol.michalowski@student.put.poznan.pl>
2017-12-29 19:13:13 +0100

    Task #5535: Obliczenie mocy oświetlenia dla pojedynczego pomieszczenia

--- src/main/java/pl/put/poznan/transformer/logic/LightingPowerCalculator.java
+++ src/main/java/pl/put/poznan/transformer/logic/LightingPowerCalculator.java
@@ -1,6 +1,7 @@
 package pl.put.poznan.transformer.logic;
 
 public class LightingPowerCalculator implements Visitor {
+
     @Override
     public void visit(Building location) {
 
@@ -13,6 +14,9 @@ public class LightingPowerCalculator implements Visitor {
 
     @Override
     public void visit(Room location) {
-
+        int bulpCount = location.getBulpCount();
+        float area = location.getArea();
+        float bulpPower = location.getBulpPower();
+        location.setLight((bulpPower * bulpCount) / area);
     }
 }
