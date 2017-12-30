e8c78d9613edf477caa6ccb344411b57c4703d09
pablo <pawel@ruminkiewicz.pl>
2017-12-30 17:09:36 +0100

    Stworzenie klasy testującej dla klasy AreaCalculator

---
+++ src/test/java/pl/put/poznan/transformer/logic/AreaCalculatorTest.java
@@ -0,0 +1,20 @@
+package pl.put.poznan.transformer.logic;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AreaCalculatorTest {
+    @Test
+    public void visit() throws Exception {
+    }
+
+    @Test
+    public void visit1() throws Exception {
+    }
+
+    @Test
+    public void visit2() throws Exception {
+    }
+
+}
