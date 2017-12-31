3b173ad6be82efb09578c164f956a434692d2fee
pablo <pawel@ruminkiewicz.pl>
2017-12-31 11:27:43 +0100

    Rozbudowanie klas testujących dla powierzchni i kubatury

--- src/test/java/pl/put/poznan/transformer/logic/AreaCalculatorTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/AreaCalculatorTest.java
@@ -11,53 +11,62 @@ import static org.junit.Assert.*;
 public class AreaCalculatorTest {
 
     AreaCalculator areaCalculator;
-    Room room1;
-    Room room2;
-    Room room3;
-    Level level;
+    Room room11;
+    Room room12;
+    Room room21;
+    Room room22;
+    Level level1;
+    Level level2;
     Building building;
-    ArrayList<Room> rooms;
+    ArrayList<Room> rooms1;
+    ArrayList<Room> rooms2;
     ArrayList<Level> levels;
 
     @Before
     public void setUp() throws Exception {
         areaCalculator = new AreaCalculator();
 
-        rooms = new ArrayList<Room>();
-        levels = new ArrayList<Level>();
+        room11 = new Room(1, "biuro", 5, 5, 3, 5, 50);
+        room12 = new Room(2, "kuchnia", 6, 6, 3, 10, 65);
+        room21 = new Room(3, "stolowka", 7, 7, 3, 5, 50);
+        room22 = new Room(4, "biuro", 8, 8, 3, 7, 55);
+        rooms1 = new ArrayList<>();
+        rooms2 = new ArrayList<>();
+        levels = new ArrayList<>();
+        rooms1.add(room11);
+        rooms1.add(room12);
+        rooms2.add(room21);
+        rooms2.add(room22);
+        level1 = new Level(1, "pietro 1", rooms1);
+        level2 = new Level(2, "pietro 2", rooms2);
+        levels.add(level1);
+        levels.add(level2);
+        building = new Building(1, "Gazownia", levels);
 
-        room1 = new Room(1,"biuro",6,6,3,4,60);
-        room2 = new Room(1,"stolowka",8,7,4,4,60);
-        room3 = new Room(1,"kuchnia",20,15,2,4,60);
-        rooms.add(room1);
-        rooms.add(room2);
-        rooms.add(room3);
-        level = new Level(1,"poziom_1",rooms);
-        levels.add(level);
-        building= new Building(1,"biurowiec",levels);
+        Visitable[] list = {room11, room12, room21, room22, level1, level2, building};
 
-        Visitable[] list = {room1,room2,room3,level,building};
-
-        for(Visitable calculation:list){
+        for (Visitable calculation : list) {
             calculation.accept(areaCalculator);
         }
     }
 
     @Test
     public void visit() throws Exception {
-        Assert.assertEquals(building.getArea(),392,0);
+        Assert.assertEquals(building.getArea(), 174, 0);
     }
 
     @Test
     public void visit1() throws Exception {
-        Assert.assertEquals(level.getArea(),392,0);
+        Assert.assertEquals(level1.getArea(), 61, 0);
+        Assert.assertEquals(level2.getArea(), 113, 0);
     }
 
     @Test
     public void visit2() throws Exception {
-        Assert.assertEquals(room1.getArea(),36,0);
-        Assert.assertEquals(room2.getArea(),56,0);
-        Assert.assertEquals(room3.getArea(),300,0);
+        Assert.assertEquals(room11.getArea(), 25, 0);
+        Assert.assertEquals(room12.getArea(), 36, 0);
+        Assert.assertEquals(room21.getArea(), 49, 0);
+        Assert.assertEquals(room22.getArea(), 64, 0);
     }
 
 }
--- src/test/java/pl/put/poznan/transformer/logic/CubeCalculatorTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/CubeCalculatorTest.java
@@ -12,10 +12,15 @@ public class CubeCalculatorTest {
 
     AreaCalculator areaCalculator;
     CubeCalculator cubeCalculator;
-    Room room;
-    Level level;
+    Room room11;
+    Room room12;
+    Room room21;
+    Room room22;
+    Level level1;
+    Level level2;
     Building building;
-    ArrayList<Room> rooms;
+    ArrayList<Room> rooms1;
+    ArrayList<Room> rooms2;
     ArrayList<Level> levels;
 
     @Before
@@ -23,35 +28,48 @@ public class CubeCalculatorTest {
         areaCalculator = new AreaCalculator();
         cubeCalculator = new CubeCalculator();
 
-        rooms = new ArrayList<Room>();
-        levels = new ArrayList<Level>();
+        room11 = new Room(1, "biuro", 5, 5, 3, 5, 50);
+        room12 = new Room(2, "kuchnia", 6, 6, 3, 10, 65);
+        room21 = new Room(3, "stolowka", 7, 7, 3, 5, 50);
+        room22 = new Room(4, "biuro", 8, 8, 3, 7, 55);
+        rooms1 = new ArrayList<>();
+        rooms2 = new ArrayList<>();
+        levels = new ArrayList<>();
+        rooms1.add(room11);
+        rooms1.add(room12);
+        rooms2.add(room21);
+        rooms2.add(room22);
+        level1 = new Level(1, "pietro 1", rooms1);
+        level2 = new Level(2, "pietro 2", rooms2);
+        levels.add(level1);
+        levels.add(level2);
+        building = new Building(1, "Gazownia", levels);
 
-        room = new Room(1,"biuro",6,6,3,4,60);
-        rooms.add(room);
-        level = new Level(1,"poziom_1",rooms);
-        levels.add(level);
-        building= new Building(1,"biurowiec",levels);
+        Visitable[] list = {room11, room12, room21, room22, level1, level2, building};
 
-        Visitable[] list = {room,level,building};
-
-        for(Visitable calculation:list){
+        for (Visitable calculation : list) {
             calculation.accept(areaCalculator);
             calculation.accept(cubeCalculator);
         }
     }
+
     @Test
     public void visit() throws Exception {
-        Assert.assertEquals(building.getCube(),108,0);
+        Assert.assertEquals(building.getCube(), 522, 0);
     }
 
     @Test
     public void visit1() throws Exception {
-        Assert.assertEquals(level.getCube(),108,0);
+        Assert.assertEquals(level1.getCube(), 183, 0);
+        Assert.assertEquals(level2.getCube(), 339, 0);
     }
 
     @Test
     public void visit2() throws Exception {
-        Assert.assertEquals(room.getCube(),108,0);
+        Assert.assertEquals(room11.getCube(), 75, 0);
+        Assert.assertEquals(room12.getCube(), 108, 0);
+        Assert.assertEquals(room21.getCube(), 147, 0);
+        Assert.assertEquals(room22.getCube(), 192, 0);
     }
 
 }
